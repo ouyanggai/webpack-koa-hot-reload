5e801f5007d5f87863593fdc7c0db112bc9e196c
ouyanggai <ouyanggai@vip.qq.com>
2023-01-27 11:17:44 +0800

    修改重载逻辑

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@
  import { WebpackPluginInstance, Compiler } from 'webpack';
  import detect from 'detect-port';
  import colors from 'colors'
+ import kill  from 'kill-port';
 
  const runCommand = (command:string, args:any) => {
   return new Promise<void>((resolve, reject) => {
@@ -55,14 +56,18 @@ class WebpackKoaHotReload implements WebpackPluginInstance{
           console.error(colors.red('******必须提供koa应用入口，请在插件调用时传递*********'))
           return 
         }
-        //编译完成 启动api服务
-        detect(this.options.port??8573).then((_port: number)=>{
-          if(this.options.port && this.options.port === _port){
-            console.log('你的koa服务已运行在:' + colors.blue.underline(`http://localhost:${this.options.port??8573}`))
-            runCommand(`nodemon --delay ${this.options.delay??0} ${this.options.main} ${this.options.port??8573}`,null)
-          }
-        }).catch((err: string)=>{
-          console.error(colors.red(err))
+        kill(this.options.port??8573).then(()=>{
+          //编译完成 启动api服务
+          detect(this.options.port??8573).then((_port: number)=>{
+            if(this.options.port && this.options.port === _port){
+              console.log('你的koa服务已运行在:' + colors.blue.underline(`http://localhost:${this.options.port??8573}`))
+              runCommand(`nodemon --delay ${this.options.delay??0} ${this.options.main} ${this.options.port??8573}`,null)
+            }
+          }).catch((err: string)=>{
+            console.error(colors.red(err))
+          })
+        }).catch(()=>{
+          console.log('重启失败')
         })
       })
     }
